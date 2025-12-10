cbc3ea1e1939b01c797772f4dde254f5c4e9627b
NicoMoguel <nicolasmm0711@gmail.com>
2025-12-09 23:07:49 -0600

    This is the first version

---
+++ calculadora.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    double a, b;
+
+    cout << "Ingrese el primer numero: ";
+    cin >> a;
+
+    cout << "Ingrese el segundo numero: ";
+    cin >> b;
+
+    double suma = a + b;
+
+    cout << "Resultado de la suma: " << suma << endl;
+
+    return 0;
+}
